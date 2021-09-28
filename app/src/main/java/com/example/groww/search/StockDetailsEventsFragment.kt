d633bc1b575fdba060d7a0255e84221625d3c166
Yash Bansal <ybansalnaveen@gmail.com>
2021-09-28 22:26:07 +0530

    #1Search

---
+++ app/src/main/java/com/example/groww/search/StockDetailsEventsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.groww.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.groww.R
+
+
+class StockDetailsEventsFragment : Fragment(R.layout.fragment_stock_details_events) {
+
+
+
+}
---
+++ app/src/main/java/com/example/groww/search/StockDetailsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.groww.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.groww.R
+
+class StockDetailsFragment : Fragment(R.layout.fragment_stock_details) {
+
+}
