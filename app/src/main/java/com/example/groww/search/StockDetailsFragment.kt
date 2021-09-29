2361b42427dd75f13011a278a58c83695a7c532b
Yash Bansal <ybansalnaveen@gmail.com>
2021-09-29 19:14:01 +0530

    #2Search

--- app/src/main/java/com/example/groww/search/StockDetailsFragment.kt
+++ app/src/main/java/com/example/groww/search/StockDetailsFragment.kt
@@ -2,11 +2,31 @@ package com.example.groww.search
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.groww.R
+import com.example.groww.search.adapter.StockDetailsFragmentAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_stock_details.*
 
 class StockDetailsFragment : Fragment(R.layout.fragment_stock_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupFragmentViewPager()
+
+    }
+
+    private fun setupFragmentViewPager() {
+        val stockDetailsFragmentAdapter = StockDetailsFragmentAdapter(childFragmentManager,lifecycle)
+        stockDetailsViewPager.adapter =stockDetailsFragmentAdapter
+            TabLayoutMediator(stockDetailsTabLayout,stockDetailsViewPager){ tab, position ->
+                when(position){
+                    0 -> tab.text = "Overview"
+                    1 -> tab.text = "News"
+                    2 -> tab.text = "Events"
+                }
+            }.attach()
+    }
 
 }
