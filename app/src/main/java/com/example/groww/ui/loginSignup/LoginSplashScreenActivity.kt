23a89984128d884dd251134c79632deb556ddf56
Aditya Cheke <adityacheke4@gmail.com>
2021-10-02 12:17:40 +0530

    FingerPrint functionality Completed

--- app/src/main/java/com/example/groww/ui/loginSignup/LoginSplashScreenActivity.kt
+++ app/src/main/java/com/example/groww/ui/loginSignup/LoginSplashScreenActivity.kt
@@ -1,50 +1,61 @@
 package com.example.groww.ui.loginSignup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.example.groww.R
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginSplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_splash_screen)
+        setContentView(R.layout.activity_logi   n_splash_screen)
 
-        runBlocking {
-            showSplashScreen1()
-//            showSplashScreen3()
+        CoroutineScope(Dispatchers.Main).launch {
+            Handler().postDelayed({
+                val fragment = SplashScreen1()
+                supportFragmentManager.commit {
+                    replace(R.id.containerView, fragment)
+                }
+            }, 2000)
+        }
+
+//        CoroutineScope(Dispatchers.Main).launch {
+//            Handler().postDelayed({
+//                val fragment2 = SplashScreen2()
+//                supportFragmentManager.commit {
+//                    replace(R.id.containerView, fragment2)
+//                }
+//            }, 1500)
+//        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val fragment2 = SplashScreen2()
+            supportFragmentManager.commit {
+                replace(R.id.containerView, fragment2)
+            }
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val fragment3 = SplashScreen3()
+            supportFragmentManager.commit {
+                replace(R.id.containerView, fragment3)
+            }
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            Handler().postDelayed({
+                val fragment = SplashScreen1()
+                supportFragmentManager.commit {
+                    replace(R.id.containerView, fragment)
+                }
+            }, 1500)
         }
     }
-
-    private suspend fun showSplashScreen1() {
-        delay(1000)
-        val fragment = SplashScreen1()
-        supportFragmentManager.commit {
-            replace(R.id.containerView, fragment)
-        }
-//        showSplashScreen2()
-    }
-
-    private suspend fun showSplashScreen2() {
-        delay(1000)
-        val fragment = SplashScreen2()
-        supportFragmentManager.commit {
-            replace(R.id.containerView, fragment)
-        }
-//        showSplashScreen3()
-    }
-
-    private suspend fun showSplashScreen3() {
-        delay(1000)
-        val fragment = SplashScreen3()
-        supportFragmentManager.commit {
-            replace(R.id.containerView, fragment)
-        }
-    }
-
 }
 
