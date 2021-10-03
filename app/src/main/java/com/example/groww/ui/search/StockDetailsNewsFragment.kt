55e244974d213b2d8a9a9d236620c2f60facf760
Yash Bansal <ybansalnaveen@gmail.com>
2021-10-03 18:41:16 +0530

    #FinalTouch

--- app/src/main/java/com/example/groww/ui/search/StockDetailsNewsFragment.kt
+++ app/src/main/java/com/example/groww/ui/search/StockDetailsNewsFragment.kt
@@ -3,21 +3,30 @@ package com.example.groww.ui.search
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.groww.R
 import com.example.groww.adapter.NewsAndEventsAdapter
 import com.example.groww.remote.responses.NewsAndEventsAPI
+import com.example.groww.viewmodel.ViewModelGrow
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_stock_details_news.*
 
 @AndroidEntryPoint
 class StockDetailsNewsFragment : Fragment(R.layout.fragment_stock_details_news) {
 
+    private val viewModelGrow: ViewModelGrow by viewModels()
+
     val newsAndEventsAPIList = mutableListOf<NewsAndEventsAPI>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setRecyclerView()
+        viewModelGrow.getNewsResponseFromApi().observe(viewLifecycleOwner, Observer {
+            newsAndEventsAPIList.clear()
+            newsAndEventsAPIList.addAll(it)
+            setRecyclerView()
+        })
 
     }
 
